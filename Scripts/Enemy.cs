96e523015ef78d25a6a9945e3c6bdf263a2ecb47
Rian101 <93387714+Rian101@users.noreply.github.com>
2021-11-20 15:18:20 +0000

    Add files via upload

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    public float speed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+
+            float moveHorizontal = direction;
+
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f);
+            rigidbody2D.velocity = movement * speed;
+
+            if (target.position.x > enemyPosition.position.x)
+            {
+
+                direction = 1;
+
+            }
+
+            if (target.position.x < enemyPosition.position.x)
+            {
+
+                direction = -1;
+
+            }
+
+
+
+    }
+
+}
---
+++ Scripts/PlayerMovement1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement1 : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float dirx = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirx * 4f, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 8f);
+        }
+        
+           
+
+    }
+}
